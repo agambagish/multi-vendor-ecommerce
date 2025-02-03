851e2b333bbfb3c945f7eaa62b21acbfe29e8684
AkashDotJS <akash06082006@gmail.com>
2025-02-03 22:32:12 +0530

    fix: 🔧 wrapped useSearchParams in suspense

--- src/app/(store)/onboarding/page.tsx
+++ src/app/(store)/onboarding/page.tsx
@@ -1,12 +1,23 @@
+import { Suspense } from "react";
+
 import {
   OnboardingMultiStepForm,
 } from "@/components/onboarding-multi-step-form";
+import { Spinner } from "@/components/spinner";
 
 export default function Page() {
   return (
-    <>
+    <Suspense fallback={(
+      <div className="flex h-[calc(100vh-5rem)] items-center justify-center">
+        <Spinner
+          variant="ellipsis"
+          className="size-8 text-muted-foreground"
+        />
+      </div>
+    )}
+    >
       <OnboardingMultiStepForm />
       <div className="absolute inset-0 top-12 -z-10 bg-cover bg-center" />
-    </>
+    </Suspense>
   );
 }
